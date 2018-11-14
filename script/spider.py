c5c106caf52acae6ce133972734177f74a857533
Tou Uki <touuki@protonmail.com>
2018-11-15 01:31:22 +0800

    update

--- script/spider.py
+++ script/spider.py
@@ -8,78 +8,76 @@ import socket
 import config
 socket.setdefaulttimeout(5)
 
-def get_weibo_client():
-	return weibo.MWeiboCn(
-		username=_cf.get('weibo','username'),
-		password=_cf.get('weibo','password'),
-		proxy_handler=get_proxy_handler(),
-		cookie_file=_cf.get('weibo','cookie_file'),
-		normal_request_interval=_cf.getint('scan','normal_request_interval'),
-		error_request_interval=_cf.getint('scan','error_request_interval'),
-		auto_retry=_cf.getboolean('scan','auto_retry'),
-		retry_times=_cf.getint('scan','retry_times')
-		)
-
 global count
 count = 0
-config.init()
-confirm_num = config.getint('scan','confirm_num')
+confirm_num = config.get_parser().getint('scan','confirm_num')
 client = config.get_weibo_client()
 if not client.st:
 	client.login()
 
+def scan_page(uid,page,min_id = 0, request_count = 1):
+	data = client.getIndex_contents(uid,page)
+	#已扫描完退出，访问太频繁有几率不返回内容，故多请求几次
+	if data['ok'] == 0:
+		if request_count < 3:
+			request_count += 1
+			time.sleep(request_count)
+			return scan_page(uid,page,min_id = min_id,request_count = request_count + 1)
+		else:
+			return False
 
+	data = data['data']
+	print("user: %s page: %s" % (uid,page))
+	for card in data['cards']:
+		if card['card_type']==9:
+			id = card['mblog']['id']
+			if int(id) < int(min_id) and 'isTop' not in card['mblog']:
+				return False
 
-def user_weibo_update(db,uid,min_id=None):
+			if 'retweeted_status' in card['mblog']:
+				retweeted_status = card['mblog']['retweeted_status']
+				#未登录情况下访问频繁会403，较久远微博retweeted_status的id等内容有时会为空
+				while not retweeted_status['id']:
+					print('retweeted_id is empty, try the other way, weibo_id: ' + id)
+					retweeted_status = get_retweeted_status(id)
+				single_weibo(retweeted_status)
+
+			single_weibo(card['mblog'])
+
+	return True
+
+def single_weibo(status):
+	if status['user'] is None:
+		print('weibo_id: {} no user, maybe deleted.')
+	else:
+		screen_name = status['user']['screen_name']
+		if not cursor.execute("SELECT id,edit_count FROM weibo_index WHERE id=%s",(status['id'],)):
+			print('{}. Updating user: {} weibo_id: {}'.format(count,screen_name,status['id']))
+			weibo_api(db,status['id'],'insert')
+		elif 'edit_count' in status:
+			_,edit_count = cursor.fetchone()
+			if edit_count < status['edit_count']:
+				print('{}. [Edited] Updating user: {} weibo_id: {}'.format(count,screen_name,status['id']))
+				weibo_api(db,status['id'],'update')
+
+def user_weibo_update(db,uid,min_id):
 	cursor = db.cursor()
 	
 	user_update(db,uid)
-	cursor.execute("SELECT id,screen_name FROM weibo_user WHERE id=%s",(uid,))
-	users = cursor.fetchall()
-
+	cursor.execute("SELECT id,max_mid FROM weibo_user WHERE id=%s",(uid,))
+	_,min_id = cursor.fetchone()
 	page = 1
-	read_count = 0
-	not_ok_count = 0
-	idcount = 0
-	while True:
-		if min_id is None and read_count >= confirm_num:
-			break
-
-		data = client.getIndex_contents(uid,page)
-		#已扫描完退出，访问太频繁有几率不返回内容，故多请求几次
-		if data['ok']==0 :
-			if not_ok_count<2 :
-				not_ok_count += 1
-				time.sleep(not_ok_count)
-				continue
-			else :
-				break
-
-		data = data['data']
-		print("user: %s page: %s" % (users[0][1],page))
-		for card in data['cards']:
-			if card['card_type']==9:
-				id = card['mblog']['id']
-				if min_id and int(id) < int(min_id):
-					if idcount>2:
-						return
-					else:
-						idcount += 1
-
-				if 'retweeted_status' in card['mblog']:
-					rid = card['mblog']['retweeted_status']['id']
-					if not cursor.execute("SELECT id FROM weibo_index WHERE id=%s",(rid,)):
-						print(str(count) + '. Updating retweeted_status: ' + users[0][1] + ' weibo_id: ' + id + "  " + rid)
-						weibo_insert(db,rid)
-
-				if not cursor.execute("SELECT id FROM weibo_index WHERE id=%s",(id,)):
-					print(str(count) + '. Updating user: ' + users[0][1] + ' weibo_id: ' + id)
-					weibo_insert(db,id)
-				else:
-					read_count += 1
-
+	while scan_page(uid,page,min_id):
 		page += 1
-		not_ok_count = 0
+
+	cursor.execute("SELECT id,uid FROM weibo_index WHERE uid=%s ORDER BY id DESC LIMIT 1")
+	max_mid,_ = cursor.fetchone()
+	try:
+		cursor.execute("UPDATE weibo_user SET max_mid=%s WHERE id=%s",(max_mid,uid))
+		db.commit()
+	except:
+		db.rollback()
+		raise
 
 
 def user_update(db,id):
@@ -116,15 +114,12 @@ def user_update(db,id):
 
 def scan_user_all(db,uid,restart=False):
 	cursor = db.cursor()
-	cursor.execute("SELECT scan_page,screen_name FROM weibo_user WHERE id=%s",(uid,))
-	users = cursor.fetchall()
-	if not users:
+	if not cursor.execute("SELECT scan_page,screen_name FROM weibo_user WHERE id=%s",(uid,)):
 		user_update(db,uid)
 		cursor.execute("SELECT scan_page,screen_name FROM weibo_user WHERE id=%s",(uid,))
-		users = cursor.fetchall()
 
 	#page为已经扫描到的页数
-	page = users[0][0]
+	page,_ = cursor.fetchone()
 
 	if page<0:
 		if restart:
@@ -134,43 +129,12 @@ def scan_user_all(db,uid,restart=False):
 	else:
 		page += 1
 
-	not_ok_count = 0
-	while True:
+	while scan_page(uid,page):
 		#获取微博列表
 		#global count
 		#count += 1
 		#if count > 800:
 		#	exit()
-
-		data = client.getIndex_contents(uid,page)
-		#已扫描完退出，访问太频繁有几率不返回内容，故多请求几次
-		if data['ok']==0 :
-			if not_ok_count<2 :
-				not_ok_count += 1
-				time.sleep(not_ok_count)
-				continue
-			else :
-				break
-		
-		data = data['data']
-		print("user: %s page: %s" % (users[0][1],page))
-		for card in data['cards']:
-			if card['card_type']==9:
-				id = card['mblog']['id']
-				if 'retweeted_status' in card['mblog']:
-					rid = card['mblog']['retweeted_status']['id']
-					while not rid:
-						print('retweeted_id is empty, try the other way, weibo_id: ' + id)
-						rid = get_retweeted_status(id)
-					cursor.execute("SELECT id FROM weibo_index WHERE id=%s",(rid,))
-					if not cursor.fetchall():
-						print(str(count) + '. Updating retweeted_status: ' + users[0][1] + ' weibo_id: ' + id + '  ' + rid)
-						weibo_insert(db,rid)
-
-				cursor.execute("SELECT id FROM weibo_index WHERE id=%s",(id,))
-				if not cursor.fetchall():
-					print(str(count) + '. Updating user: ' + users[0][1] + ' weibo_id: ' + id)
-					weibo_insert(db,id)
 
 		#更新扫描页面
 		try:
@@ -180,7 +144,7 @@ def scan_user_all(db,uid,restart=False):
 			db.rollback()
 			raise
 		page += 1
-		not_ok_count = 0
+
 	#扫描完后scan_page设为负
 	try:
 		cursor.execute("UPDATE weibo_user SET scan_page=%s WHERE id=%s",(-page+1,uid))
@@ -195,25 +159,23 @@ def get_retweeted_status(id):
 		raise Exception('data error')
 	else:
 		if 'retweeted_status' in data['status']:
-			return data['status']['retweeted_status']['id']
+			return data['status']['retweeted_status']
 		else:
 			return None
 	
 
-def weibo_insert(db,id):
+def weibo_api(db,id,operation='insert'):
 
 	global count
 	count += 1
 
 	cursor = db.cursor()
-	#未登录情况下访问频繁会403，已登录情况下访问频繁retweeted_status的id等内容会为空
-
 	content = client.status(id,decode=False)
 	if content is not None:
-		with urllib.request.urlopen('http://localhost/weibo/api/insert_weibo.php',urllib.parse.urlencode({"data":content}).encode("ascii")) as f:
+		with urllib.request.urlopen('http://localhost/weibo/api/{}_weibo.php'.format(operation),urllib.parse.urlencode({"data":content}).encode("ascii")) as f:
 			reponse = f.read().decode()
-			insert_result = json.loads(reponse)
-			if insert_result['errorCode'] != "0" and insert_result['errorCode'] != "101":
+			result = json.loads(reponse)
+			if result['errorCode'] != "0" and result['errorCode'] != "101":
 				raise Exception(reponse)
 
 if __name__ == '__main__':
--- script/test/test.py
+++ script/test/test.py
@@ -1,11 +1,1 @@
-import urllib.request
-x = 1
-
-def f2():
-	return x
-
-def f1(a=f2()):
-	print(a)
-
-x = 2
-f1()
+print('{}www%s'.format(111))
